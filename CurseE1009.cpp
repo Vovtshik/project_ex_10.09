bb7cc14eca816b10d6a76c5cc323bd20cfd168e6
Volodymir Konovalov <vvkon@meta.ua>
2019-09-16 22:24:20 +0300

    Update CurseE1009.cpp
    Добавлены определения
    функций in_file и out_file

--- CurseE1009.cpp
+++ CurseE1009.cpp
@@ -10,3 +10,27 @@ int main()
     
 	return 0;
 }
+
+void in_file(vector<string>& vs, string& name)
+{
+	ifstream ist(name);
+	if(!ist) error("Невозможно открыть входной файл", name);
+	string temp{};
+	while(true)
+	{
+		ist >> temp;
+		if(ist)
+			vs.push_back(temp);
+		if(ist.eof())
+			return;
+	}
+}
+
+void out_file(vector<string>& vs, string& name)
+{
+	ofstream ost(name);
+	if(!ost) error("Невозможно открыть выходной файл", name);
+	string temp{};
+	for(string& x: vs)
+		ost << x << " ";
+}
